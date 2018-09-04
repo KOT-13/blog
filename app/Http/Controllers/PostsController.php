decf19d4571d4d1bc0540b321a4ddf32ac5a808e
alexn <anazarenco@best4u.md>
2018-09-04 20:27:31 +0300

    docker + docker-compose

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show()
+    {
+        return view('posts.show');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        $this->validate(request(), [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        Post::create([
+            'title' => request('title'),
+            'body' => request('body')
+        ]);
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h1>Place to show content</h1>
+
+@endsection
