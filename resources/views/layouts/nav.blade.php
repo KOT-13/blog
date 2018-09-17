601d29707daa5ac4be4b78cff4e816daf9dc83df
alexn <anazarenco@best4u.md>
2018-09-17 11:47:53 +0300

    nav fixed

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,11 +2,16 @@
     <div class="container">
         <nav class="nav blog-nav">
             <a class="nav-link active" href="/">Home</a>
-            <a class="nav-link" href="#">New features</a>
-            <a class="nav-link" href="#">Press</a>
-            <a class="nav-link" href="#">New hires</a>
+
+            @if(! Auth::check())
+                <a class="nav-link ml-auto" href="/login">Login</a>
+                <a class="nav-link" href="/register">Registration</a>
+            @endif
+
             @if(Auth::check())
+                <a class="nav-link" href="/posts/create">Create a post</a>
                 <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
+                <a class="nav-link" href="/logout">Logout</a>
             @endif
         </nav>
     </div>
